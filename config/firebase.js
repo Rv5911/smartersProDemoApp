f94bc1055156281d6a0b7e062b5a8a78ff1f0000
Ranbir <ranbirverma.25@techsmarters.com>
2026-03-20 09:48:48 +0530

    code-refactor

--- config/firebase.js
+++ config/firebase.js
@@ -18,6 +18,7 @@ window.logAllDnsEntries = function () {
     .then(function (snapshot) {
       snapshot.forEach(function (doc) {
         // console.log("DNS Entry:", doc.id, "=>", doc.data());
+        
         localStorage.setItem("all_dns", JSON.stringify(doc.data().DNS));
       });
 
